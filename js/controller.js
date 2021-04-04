a43ac71a8ab8be139357d84c932d57416ba4d7ef
Clement TURMEL <clemturmel@gmail.com>
2021-04-04 20:33:52 +0200

    handle only game keys, F5 or F12

--- js/controller.js
+++ js/controller.js
@@ -4,7 +4,7 @@ const UP = 'up';
 const DOWN = 'down';
 const SPACE = 'space';
 
-const keys = {
+const game_keys = {
     37: LEFT,
     39: RIGHT,
     38: UP,
@@ -12,19 +12,32 @@ const keys = {
     32: SPACE
 }
 
+
+const tech_keys = {
+    116: 'F5',
+    123: 'F12',
+}
+
 document.body.onkeydown = function( e ) {
 
-    console.log("key pressed is : " + e.keyCode + " -> " + keys[e.keyCode]);
-    if (isKnownKeys) {
-        e.preventDefault(); //avoid moving/scrolling page
+    console.log("key pressed is : " + e.keyCode + " -> " + game_keys[e.keyCode]);
+    if (!isKnownKeys(e.keyCode) && !isKnownTechKeys(e.keyCode)) {
+        console.log("NOP ! ");
+        e.preventDefault();
+    }else{ 
+        console.log("this is a known key ! ");
+        //e.preventDefault(); //avoid moving/scrolling page
         
-        if(!isGamePaused || keys[ e.keyCode ] == SPACE){
-            onkeyPress( keys[ e.keyCode ] );
+        if(!isGamePaused || game_keys[ e.keyCode ] == SPACE){
+            onkeyPress( game_keys[ e.keyCode ] );
         }
-        
     }
 };
 
 function isKnownKeys(keyPressed){
-    return typeof keys[ keyPressed.keyCode ] != 'undefined';
+    return typeof game_keys[ keyPressed ] != 'undefined';
+}
+
+function isKnownTechKeys(keyPressed){
+    return typeof tech_keys[ keyPressed ] != 'undefined';
 }
